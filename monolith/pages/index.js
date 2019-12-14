8670b9f6371f269bb0c64d54ff8796d9c7fd97ba
Soeren <soeren.vu@gmail.com>
2019-12-14 18:28:28 +0100

    bissl rumspielen

--- monolith/pages/index.js
+++ monolith/pages/index.js
@@ -1,7 +1,70 @@
-import { HomePage } from 'micro-frontend-react-pages';
-
 import 'micro-frontend-react-pages/src/scss/pages/home.scss';
 
-const Index = () => <HomePage />
+const Index = () => <div>
+  <header className={"home-page-header"}>
+    <div className={"home-page-menu"}>
+      <div className={"home-page-menu-item"}>
+        <a href={"#"} className={"home-page-menu-item-link"}>Welcome to the Beerside of things</a>
+      </div>
+    </div>
+  </header>
+  <section className={"first-section"}>
+    <div className={"first-section-wrapper"}>
+      <h1 className={"first-section-title"}>We currently have NUMBER beers</h1>
+      <p className={"first-section-description"}>But choose wisely!</p>
+    </div>
+  </section>
+  <section className={"home-page-wrapper is-1-column"}>
+    <h1 className={"home-page-heading"}>Beer of the day</h1>
+    <div>
+      <h4>Barre Altschuss</h4>
+      <span>Beschreibung....</span>
+    </div>
+    <hr style={{
+      display: 'block',
+      height: '1px',
+      border: '0',
+      borderTop: '1px solid #ccc',
+      margin: '4px 0',
+      padding: '0'
+    }}/>
+    <div>
+      <h1 className={"home-page-heading"}>List of our awesome beers</h1>
+      <table align={"center"} style={{
+        fontFamily: "Trebuchet MS, Arial, Helvetica, sans-seri",
+        borderCollapse: 'collapse',
+        width: '100%'
+      }}>
+        <thead style={{
+          paddingTop: '4px',
+          paddingBottom: '4px',
+          textAlign: 'center',
+          backgroundColor: '#4CAF50',
+          color: 'white'
+        }}>
+          <tr>
+            <th>Name</th>
+            <th>Tagline</th>
+            <th>Image</th>
+            <th>First Brewed</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody align={"center"} style={{
+          border: '1px solid #ddd',
+          padding: '8px'
+        }}>
+          <tr>
+            <td>Barre</td>
+            <td>Barre Bräu dein Herz erfreu!</td>
+            <td>nope</td>
+            <td>1968</td>
+            <td>Beschte</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</div>;
 
 export default Index;
--- monolith/server.js
+++ monolith/server.js
@@ -1,7 +1,74 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const port = parseInt(process.env.PORT || 3000, 10);
 const isProduction = process.env.NODE_ENV === 'production';
+
+function createMongoConnectionString(
+  user = process.env.DB_USER,
+  password = process.env.DB_PASS,
+  connectionUrl = '@punkbeer-ggdxi.mongodb.net/punkbeer?retryWrites=true&w=majority',
+) {
+  return 'mongodb+srv://' + user + ':' + password + connectionUrl;
+}
+
+function connect() {
+  console.log('connecting to mongo ...');
+  return mongoose.connect(createMongoConnectionString(), { useNewUrlParser: true, useUnifiedTopology: true });
+}
+
+const BeerSchema = new Schema({
+  food_pairing: [String],
+  name: String,
+  tagline: String,
+  first_brewed: String,
+  description: String,
+  image_url: String,
+  abv: Number,
+  ibu: Number,
+  target_fg: Number,
+  target_og: Number,
+  ebc: Number,
+  srm: Number,
+  ph: Number,
+  attenuation_level: Number,
+  volume: {
+    value: Number,
+    unit: String
+  },
+  boil_volume: {
+    value: Number,
+    unit: String
+  },
+  method: {
+    mash_temp: [
+      {
+        temp: {
+          value: Number,
+          unit: String
+        },
+        duration: Number
+      }
+    ],
+    fermentation: {
+      temp: {
+        value: Number,
+        unit: String
+      }
+    },
+  },
+  ingredients: {
+    malt: [{name: String, amount: {value: Number, unit: String}}],
+    hops: [{name: String, amount: {value: Number, unit: String}, add: String, attribute: String}],
+    yeast: String
+  },
+  brewers_tips: String,
+  contributed_by: String
+});
 
 const app = next({
     isProduction,
@@ -11,8 +78,9 @@ const app = next({
 
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
+app.prepare().then(() => connect()).then(() => {
     const server = express();
+    const Beer = mongoose.model('beer', BeerSchema);
 
     server.get('/items', (req, res) => app.render(req, res, '/items'));
 
@@ -22,14 +90,20 @@ app.prepare().then(() => {
 
     server.get('/checkout', (req, res) => app.render(req, res, '/checkout'));
     server.get('/docs', (req, res) => app.render(req, res, '/docs'));
-    server.get('/home', (req, res) => app.render(req, res, '/'));
+
+    server.get('/home', async (req, res) => {
+      const beers = await Beer.find({});
+      console.log(beers);
+      return app.render(req, res, '/');
+    });
+
     server.get('/', (req, res) => app.render(req, res, '/'));
 
     server.get('/*', (req, res) => handle(req, res));
 
     server.listen(port, err => {
-        if(err) throw err
+        if(err) throw err;
         console.log(`> Read on http://localhost:${port}`);
     })
-});
+}).catch(err => console.error(err));
 
